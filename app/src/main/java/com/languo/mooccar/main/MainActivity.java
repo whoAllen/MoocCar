4384aa843c5973f0fc772fe2e2bc324c91a6d435
lg_yuliang <2042601376@qq.com>
2018-01-07 13:42:27 +0800

    补全之前的 SplashActivity ，使用的是矢量动画

--- app/src/main/java/com/languo/mooccar/main/MainActivity.java
+++ app/src/main/java/com/languo/mooccar/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.languo.mooccar;
+package com.languo.mooccar.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.languo.mooccar.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/languo/mooccar/splash/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.languo.mooccar.splash;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.languo.mooccar.R;
+import com.languo.mooccar.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            final AnimatedVectorDrawable anim =
+                    (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.anim);
+            final ImageView logo = (ImageView) findViewById(R.id.logo);
+            logo.setImageDrawable(anim);
+            anim.start();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toMainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(toMainIntent);
+            }
+        }, 3000);
+    }
+}
